403116e3fe3d0bd2cd480ebcbf15421503d5f7b2
Saurav Kumar <csaurav2014@gmail.com>
2026-02-25 17:21:19 +0530

    Setiing up User Service , User Entity and User Database

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.fitness.userservice.controller;
+
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserProfiles(userId));
+    }
+
+    @PostMapping("/{register}")
+    public ResponseEntity<UserResponse> userRegister(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.userRegister(request));
+    }
+
+}
---
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,11 @@
+package com.fitness.userservice.service;
+
+import com.fitness.userservice.dto.UserResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public @Nullable UserResponse getUserProfiles(String userId) {
+    }
+}
