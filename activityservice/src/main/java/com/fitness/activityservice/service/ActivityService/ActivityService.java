55ac231395d6cd4b91ea3d56a169a6505e9c6444
Saurav Kumar <csaurav2014@gmail.com>
2026-03-07 19:33:58 +0530

    Build Activity model, Dto, Service, Controller and Repository. Also solve created and updatedAt null problem in storing data in MongoDB database by using config class with Configuration and EnableMongoAuditing annotation

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService/ActivityService.java
@@ -0,0 +1,46 @@
+package com.fitness.activityservice.service.ActivityService;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity savedActivity) {
+        ActivityResponse activityResponse = new ActivityResponse();
+        activityResponse.setId(savedActivity.getId());
+        activityResponse.setUserId(savedActivity.getUserId());
+        activityResponse.setType(savedActivity.getType());
+        activityResponse.setDuration(savedActivity.getDuration());
+        activityResponse.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        activityResponse.setStartTime(savedActivity.getStartTime());
+        activityResponse.setAdditionalMetrics(savedActivity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(savedActivity.getCreatedAt());
+        activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
+        return activityResponse;
+    }
+}
