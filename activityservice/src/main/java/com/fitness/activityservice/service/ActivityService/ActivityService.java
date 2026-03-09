446ddcb836adfc2905475a4ca1ffc495d3118894
Saurav Kumar <csaurav2014@gmail.com>
2026-03-09 17:57:27 +0530

    Create API for Getting User Activity by activity ID

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService/ActivityService.java
@@ -6,7 +6,12 @@ import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +35,17 @@ public class ActivityService {
         return mapToResponse(savedActivity);
     }
 
+    public List<ActivityResponse> getUserActivities(String userId) {
+
+        List<Activity> activities = activityRepository.findByUserId(userId);
+
+        List<ActivityResponse> activitiesResponses = activities.stream().map(activity -> mapToResponse(activity)).collect(Collectors.toList());
+
+        return activitiesResponses;
+    }
+
+
+
     private ActivityResponse mapToResponse(Activity savedActivity) {
         ActivityResponse activityResponse = new ActivityResponse();
         activityResponse.setId(savedActivity.getId());
@@ -42,5 +58,11 @@ public class ActivityService {
         activityResponse.setCreatedAt(savedActivity.getCreatedAt());
         activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
         return activityResponse;
+    }
+
+
+    public ActivityResponse getActivityById(String activityId) {
+
+        return activityRepository.findById(activityId).map(this::mapToResponse).orElseThrow(() -> new RuntimeException("Activity not found with id: " + activityId));
     }
 }
